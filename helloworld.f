0625efb2acd2e3c14da5f13b5702bb52c0129fb1
KJ Sweet <dragonrune108@gmail.com>
2015-05-06 14:45:56 -0400

    Updated indenting
    arrrrgh Fortran

--- helloworld.f
+++ helloworld.f
@@ -1,12 +1,13 @@
 	program HELLOWORLD
-C This program does the thing. I may add more later. Like loops. Just because.
-C I did. I added a loop.
+C 	This program does the thing. I may add more later. Like loops. Just because.
+C 	I did. I added a loop.
+C 	I'm not totally sure how I want indenting to go, so I made it 8 just in case. Because Fortran is awful.
 
-  implicit none
-  integer i
+	implicit none
+	integer i
   
-  do i = 1, 4
-  	print *, 'Hello World!'
-  enddo
+	do i = 1, 4
+  		print *, 'Hello World!'
+	enddo
   
-  end
+	end
