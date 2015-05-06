518ef5a9dffe2541522c165bc2904f553a7be740
KJ Sweet <dragonrune108@gmail.com>
2015-05-06 14:42:19 -0400

    Added a loop
    Fortran is better than Java because it's smart enough to increase the integer counter on the loop by itself. Fight me.

--- helloworld.f
+++ helloworld.f
@@ -1,6 +1,12 @@
-  program HELLOWORLD
+	program HELLOWORLD
 C This program does the thing. I may add more later. Like loops. Just because.
+C I did. I added a loop.
+
+  implicit none
+  integer i
   
-  print *, 'Hello World!'
+  do i = 1, 4
+  	print *, 'Hello World!'
+  enddo
   
   end
